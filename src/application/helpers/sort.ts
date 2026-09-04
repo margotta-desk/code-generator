7c5d7a4bcd20c65dc060362058f502342f3c9209
Roberto Saez Margotta <margotta@gmail.com>
2026-09-04 09:13:10 -0400

    Fix menor

--- src/application/helpers/sort.ts
+++ src/application/helpers/sort.ts
@@ -12,19 +12,15 @@ export class Sort {
 	}
 
 	public static RecordArrayByKey = <T>(arg: Record<string, T[]>): Record<string, T[]> => {
-		
-		//let packages: string[] = Object.keys(arg).filter(f => f.startsWith('@packages/'))
 		let internal: string[] = Object.keys(arg).filter(f => f.startsWith('.'))
 		let others: string[] = Object.keys(arg).filter(f => !internal.includes(f))
 		
-		//packages.sort((a: string, b: string) => a > b ? 1 : -1)
 		internal.sort((a: string, b: string) => a > b ? 1 : -1)
 		others.sort((a: string, b: string) => a > b ? 1 : -1)
 		
 		const result: Record<string, T[]> = {}
-		others.forEach((m: string) => result[m] = arg[m].sort((a, b) => a < b ? -1 : 1))
-		//packages.forEach((m: string) => result[m] = arg[m].sort((a, b) => a < b ? -1 : 1))
-		internal.forEach((m: string) => result[m] = arg[m].sort((a, b) => a < b ? -1 : 1))
+		others.forEach((m: string) => result[m] = arg[m].sort((a, b) => a > b ? 1 : -1))
+		internal.forEach((m: string) => result[m] = arg[m].sort((a, b) => a > b ? 1 : -1))
 
 		return result
 	}
